c3eb7871b35bff459447f4c4e2dad7d744db6a66
Shahbaaz <shahbaazc@gmail.com>
2022-03-03 13:08:04 +0530

    added wrapper

---
+++ src/Components/Helpers/Wrappers.js
@@ -0,0 +1,8 @@
+/** ************************************************ */
+/** This function used for to avoid the unnecessary <div> tags called as wrapper  */
+/** ************************************************ */
+
+const Wrappers = (props) => {
+    return props.children;
+}
+export default Wrappers;
